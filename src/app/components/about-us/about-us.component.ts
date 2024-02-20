fae7c9d47fd7ad0770bf2e5cbec280bac1a798db
carolangel19 <102700849+CarolAngel19@users.noreply.github.com>
2024-02-20 01:20:04 -0500

    Actualizaciones sprint-2 archivo .md

--- src/app/components/about-us/about-us.component.ts
+++ src/app/components/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-about-us',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent {
+  contactForm: FormGroup;
 
+  constructor(private formBuilder: FormBuilder) {
+    this.contactForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      message: ['', Validators.required]
+    });
+  }
+
+  onSubmit() {
+    if (this.contactForm.valid) {
+      const name = this.contactForm.value.name;
+      alert(`Gracias por contactarnos, ${name}!`);
+      this.contactForm.reset();
+    }
+  }
 }
